bbae7d2877953a31e0a6e6bc6c913c2809d327da
eddiemuhoro <eddiemuhoro@gmail.com>
2025-05-17 01:36:03 +0300

    Add 'rest_framework' to INSTALLED_APPS in settings.py

--- django_stripe_api/settings.py
+++ django_stripe_api/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api'
+    'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
